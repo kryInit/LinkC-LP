f707c58eeb6f15f658bf5c32b94c00f48694b47d
kryInit <kryinit@gmail.com>
2023-09-14 23:58:55 +0900

    edit config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/pencil-LP-sample/",
+    base: "/LinkC-LP/",
     server: {
         host: "0.0.0.0",
     },
